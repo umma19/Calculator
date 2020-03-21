21670e67e306d79f76f47f4e97064aacea9d6eaf
MaryamRezaei <maryam.rezaei19870921@gmail.com>
2020-03-21 21:17:28 +0100

    Delete 2 other lines for test.

--- Calculator/src/com/bth/main/Calculator.java
+++ Calculator/src/com/bth/main/Calculator.java
@@ -2,11 +2,9 @@ package com.bth.main;
 
 public class Calculator {
 	public static int addition(int a, int b) {
-	
 		return a + b;
 	}
 	public static int subtraction(int from, int number) {
-		
 		return from - number;
 	}
 	public  static int divide(int a, int b) {
